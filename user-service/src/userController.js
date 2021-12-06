97fb4753f8940675c1cc9833b8ab21eaa8a674ee
Wojciech Pilar <astan2804@gmail.com>
2021-12-06 13:22:42 +0100

    register fixed, login added

--- user-service/src/userController.js
+++ user-service/src/userController.js
@@ -1,22 +1,23 @@
 'use strict';
 
-// const fastify = require('fastify')();
-// let users = require('./Users')
-// const { v4: uuidv4 } = require('uuid');
 const config = require('config');
 
 const { mongoUrl } = config;
 
 const mongoose = require('mongoose');
 
-const User = require('./schemas');
+const md5 = require('crypto-js/md5');
+const jwt = require('jsonwebtoken');
+
+const User = require('./User');
+
+const secret = process.env.JWT_SECRET || 'secret';
 
 mongoose.connect(mongoUrl);
 
-// User.findById('61a60c383cb13d0ca5e99a59');
-
 const getUsers = async (req, reply) => {
   // const users = await fastify.mongoDb.collection('users');
+  const users = await User.find();
   reply.send(users);
 };
 
@@ -28,13 +29,25 @@ const getUser = async (req, reply) => {
   reply.send(user);
 };
 
-const registerUser = (req, reply) => {
+const registerUser = async (req, reply) => {
   const {
     name, password, email, role, course, semester, group
   } = req.body;
+
+  const [userByName] = await User.find({ name });
+  if (userByName && userByName.id) {
+    throw new Error('User with this name already exists');
+  }
+
+  const [userByEmail] = await User.find({ email });
+  if (userByEmail && userByEmail.id) {
+    throw new Error('User with this email already exists');
+  }
+
+  const hashedPassword = md5(password).toString();
   const user = new User({
     name,
-    password,
+    password: hashedPassword,
     email,
     role,
     course,
@@ -46,10 +59,43 @@ const registerUser = (req, reply) => {
   reply.code(201).send(user);
 };
 
+const loginUser = async (req, reply) => {
+  const {
+    name, password
+  } = req.body;
+  const hashedPassword = md5(password).toString();
+  const [user] = await User.find({ name, password: hashedPassword });
+
+  if (!user || !user.id) {
+    throw new Error('User with such credentials does not exist');
+  }
+
+  const token = jwt.sign({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    course: user.course,
+    semester: user.semester,
+    group: user.group
+  }, secret);
+
+  return reply.send({
+    token
+  });
+};
+
 const deleteUser = (req, reply) => {
   const { id } = req.params;
 
-  const users = users.filter(user => user.id !== id);
+  // const users = users.filter(user => user.id !== id);
+  User.findByIdAndDelete(id, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Removed User : ', docs);
+    }
+  });
   reply.send({ message: `User ${id} has been removed` });
 };
 
@@ -58,11 +104,23 @@ const updateUser = (req, reply) => {
   const {
     name, password, email, role, course, semester, group
   } = req.body;
-  const users = users.map(user => (user.id === id ? {
-    id, name, password, email, role, course, semester, group
-  } : user));
-  const user = users.find(user => user.id === id);
-  reply.send(user);
+  // const users = users.map(user => (user.id === id ? {
+  //   id, name, password, email, role, course, semester, group
+  // } : user));
+  // const user = users.find(user => user.id === id);
+
+  User.findById(id).then(user => {
+    user.name = name;
+    user.password = password;
+    user.email = email;
+    user.role = role;
+    user.role = role;
+    user.course = course;
+    user.semester = semester;
+    user.group = group;
+    user.save();
+    reply.send(user);
+  });
 };
 
 module.exports = {
@@ -70,5 +128,6 @@ module.exports = {
   getUsers,
   registerUser,
   deleteUser,
-  updateUser
+  updateUser,
+  loginUser
 };
